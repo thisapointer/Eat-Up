ae36aa677fcf68723dbe3e531f0b08a376dffb85
thisapointer <kjw02050@naver.com>
2026-07-30 14:19:38 +0900

    fix: 브레이크타임 코드 수정

--- backend/app/services/breaks.py
+++ backend/app/services/breaks.py
@@ -33,10 +33,13 @@ def create(break_info: list[BreakBase],
     # 영업시간 존재하지 않을 시 에러
     if not hour:
         raise HTTPException(status_code=404, detail="Rest or RestHour not found")
+    # 브레이크타임 존재할 시 에러
+    if hour.breaks:
+        raise HTTPException(status_code=409, detail="Breaktime already exists")
     
     # 브레이크타임 반복 등록
     for break_item in break_info:
-        new_break = Break(**break_item.model_dump(), rest_id=rest_id, weekday=weekday)
+        new_break = Break(**break_item.model_dump(), weekday=weekday)
         hour.breaks.append(new_break)
 
     db.commit()     # DB에 반영
@@ -47,38 +50,50 @@ def create(break_info: list[BreakBase],
 def replace(break_info: list[BreakBase], 
            rest_id: int, weekday: str, db: Session) -> list[Break]:
     
-    # DB에서 식당 조회
-    rest = db.query(Rest).filter(Rest.id == rest_id).first()
-    # 식당 존재하지 않을 시 에러
-    if not rest:
-        raise HTTPException(status_code=404, detail="Rest not found")
+    # 해당 영업시간
+    hour = db.query(RestHour)\
+            .options(selectinload(RestHour.breaks))\
+            .filter(RestHour.rest_id == rest_id)\
+            .filter(RestHour.weekday == weekday).first()
+    # 영업시간 존재하지 않을 시 에러
+    if not hour:
+        raise HTTPException(status_code=404, detail="Rest or RestHour not found")
 
     # 전체 브레이크타임 삭제
-    db.query(Break)\
-        .filter(Break.rest_id == rest_id)\
-        .filter(Break.weekday == weekday).delete()
+    hour.breaks.clear()
 
     # 브레이크타임 반복 등록
-    new_breaks = []
     for break_item in break_info:
-        new_break = Break(**break_item.model_dump(), rest_id=rest_id, weekday=weekday)
-        db.add(new_break)
-        new_breaks.append(new_break)    # 리턴용 리스트에 보관
-
-    db.commit()     # DB에 반영
+        new_break = Break(**break_item.model_dump(), weekday=weekday)
+        hour.breaks.append(new_break)
     
-    return new_breaks
+    # DB에 반영    
+    db.commit()
+    
+    return hour.breaks
 
 # 브레이크 타임 삭제 로직
 def delete(break_id: int, rest_id: int, weekday: str,
            db: Session) -> bool:
-    
-    # 브레이크타임 삭제
-    db.query(Break)\
-        .filter(Break.rest_id == rest_id)\
-        .filter(Break.weekday == weekday)\
-        .filter(Break.id == break_id).delete()
-    
+
+    # 해당 영업시간
+    hour = db.query(RestHour)\
+            .options(selectinload(RestHour.breaks))\
+            .filter(RestHour.rest_id == rest_id)\
+            .filter(RestHour.weekday == weekday).first()
+    # 영업시간 존재하지 않을 시 에러
+    if not hour:
+        raise HTTPException(status_code=404, detail="Rest or RestHour not found")
+
+    # 해당 브레이크 타임
+    target_break = next((b for b in hour.breaks if b.id == break_id), None)
+    # 삭제할 브레이크타임이 존재하지 않을 경우 에러
+    if not target_break:
+        raise HTTPException(status_code=404, detail="Breaktime not found")
+
+    # 리스트에서 제거
+    hour.breaks.remove(target_break)
+
     # DB에 반영
     db.commit()
 
