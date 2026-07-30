c0384cb38786c612e7aca26e2524f3ea6dcfbc38
thisapointer <kjw02050@naver.com>
2026-07-30 15:01:44 +0900

    test: 유저 프로필 조회 테스트 코드 작성

--- backend/tests/test_routers/users/test_get_user.py
+++ backend/tests/test_routers/users/test_get_user.py
@@ -4,7 +4,7 @@ from app.models.users import User
 
 
 # ============= 성공 코드 ===============
-# 유저 전체 조회 성공 테스트 코드
+# 유저 전체 조회
 @pytest.mark.skip(reason="검증 완료")
 def test_get_all_user_success(client, db):
 
@@ -30,7 +30,7 @@ def test_get_all_user_success(client, db):
     users = db.query(User).all()
     assert users is not None
 
-# 유저 조회 성공 테스트 코드
+# 유저 조회
 @pytest.mark.skip(reason="검증 완료")
 def test_get_user_success(client, db):
 
@@ -55,6 +55,28 @@ def test_get_user_success(client, db):
     user_result = db.query(User).filter(User.id == user.id).first()
     assert user_result is not None
 
+# 유저 프로필 조회
+@pytest.mark.skip(reason="검증 완료")
+def test_get_profile_success(client, auth_headers, test_user, db):
+
+    # GIVEN
+
+
+    # WHEN
+    response = client.get(f"/api/v1/users/me", headers=auth_headers)
+
+
+    # THEN
+    print("\n================ [응답 결과] ================")
+    print("Status Code :", response.status_code)
+    print("Response JSON:", response.json())
+    print("=============================================\n")
+
+    assert response.status_code == 200
+
+    user = db.query(User).filter(User.user_id == test_user.user_id).first()
+    assert user.nickname == test_user.nickname
+
 
 # ============= 유저 조회 실패 코드 ==============
 # 존재하지 않는 유저 id 
@@ -78,3 +100,24 @@ def test_get_user_fail(client, db):
 
     user_result = db.query(User).filter(User.id == 3).first()
     assert user_result is None
+
+# 토큰 없이 유저 프로필 조회
+@pytest.mark.skip(reason="검증 완료")
+def test_get_profile_without_token(client):
+
+    # GIVEN
+
+
+    # WHEN
+    response = client.get(f"/api/v1/users/me")
+
+
+    # THEN
+    print("\n================ [응답 결과] ================")
+    print("Status Code :", response.status_code)
+    print("Response JSON:", response.json())
+    print("=============================================\n")
+
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Not authenticated"
+
