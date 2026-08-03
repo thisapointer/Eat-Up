9de34fa7be970695223a69210a55106056f18c38
thisapointer <kjw02050@naver.com>
2026-08-03 16:36:28 +0900

    fix: 브레이크타임 복합 외래키 제약조건 설정

--- backend/app/models/breaks.py
+++ backend/app/models/breaks.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Integer, DateTime, Time, ForeignKey, func, Enum as SQLEnum
+from sqlalchemy import Integer, DateTime, Time, ForeignKeyConstraint, func, Enum as SQLEnum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime, time
-from enum import IntEnum
 
 from app.database.base import Base
 from app.models.rest_hours import Weekday
@@ -17,9 +16,19 @@ class Break(Base):
     break_start_time: Mapped[time] = mapped_column(Time)    # 브레이크 시작 시간
     break_end_time: Mapped[time] = mapped_column(Time)      # 브레이크 종료 시간
 
+    # 식당 id (외래키)
+    rest_id: Mapped[int] = mapped_column(Integer, nullable=False)
     # 식당 영업시간 id (외래키)
-    weekday: Mapped[Weekday] = mapped_column(SQLEnum(Weekday, native_enum=False), ForeignKey("rest_hours.weekday", ondelete="CASCADE"), nullable=False)
+    weekday: Mapped[Weekday] = mapped_column(SQLEnum(Weekday, native_enum=False), nullable=False)
     # 식당 영업시간 역참조 설정
     rest_hour: Mapped["RestHour"] = relationship("RestHour", back_populates="breaks")
 
+    # 복합 외래키(ForeignKeyConstraint) 제약조건 설정
+    __table_args__ = (
+        ForeignKeyConstraint(
+            ["rest_id", "weekday"],                         # Break 테이블의 컬럼들
+            ["rest_hours.rest_id", "rest_hours.weekday"],   # 참조할 RestHour 테이블의 컬럼들
+            ondelete="CASCADE"
+        ),
+    )
     
