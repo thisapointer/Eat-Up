ef5384673af6c5ae3cdc3a2d82227125dc36dde5
thisapointer <kjw02050@naver.com>
2026-08-03 17:00:43 +0900

    fix: rest_id와 weekday 순서 변경

--- backend/app/models/rest_hours.py
+++ backend/app/models/rest_hours.py
@@ -18,6 +18,10 @@ class Weekday(str, Enum):
 # 식당 영업정보 ORM 모델
 class RestHour(Base):
     __tablename__ = "rest_hours"   # 연결 테이블 이름: rest_hours
+
+    # 식당 id (외래키)
+    rest_id: Mapped[int] = mapped_column(Integer, ForeignKey("rests.id", ondelete="CASCADE"), nullable=False, primary_key=True)
+    
     weekday: Mapped[Weekday] = mapped_column(SQLEnum(Weekday, native_enum=False), primary_key=True, nullable=False)     # 요일 (DB 식별자)
     created: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())      # 생성 시간
     updated: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())  # 수정 시간
@@ -26,8 +30,6 @@ class RestHour(Base):
     open_time: Mapped[time | None] = mapped_column(Time)       # 영업 시작 시간
     close_time: Mapped[time | None] = mapped_column(Time)      # 영업 종료 시간
 
-    # 식당 id (외래키)
-    rest_id: Mapped[int] = mapped_column(Integer, ForeignKey("rests.id", ondelete="CASCADE"), nullable=False, primary_key=True)
     # 식당 역참조 설정
     rest: Mapped["Rest"] = relationship("Rest", back_populates="rest_hours")
 
